d61c9c15377ce194d855b7694fbe816e94288b9b
Leonardo Guerra <103969568+oleoz1n@users.noreply.github.com>
2024-05-20 14:44:14 -0300

    Atualizar o serverUrl.ts

--- utils/serverUrl.ts
+++ utils/serverUrl.ts
@@ -1,4 +1,3 @@
-export const url = "http://10.0.2.2" 
-//export const serverUrl = 'https://7d21-2804-14c-a0-8558-51c0-dd63-c851-bd67.ngrok-free.app';
+export const serverUrl = "http://10.0.2.2" 
 
     
